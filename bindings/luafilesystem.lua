ea9557e9f2ca03e43bce5f9123bcd23360408729
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-04-24 16:39:26 +0200

    Add luafilesystem bindings

---
+++ bindings/luafilesystem.lua
@@ -0,0 +1,17 @@
+local lfs = require("lfs") or {}
+
+return {
+	["attributes!"] = lfs.attributes,
+	["ch-dir!"] = lfs.chdir,
+	["lock-dir!"] = lfs.lock_dir,
+	["current-dir!"] = lfs.currentdir,
+	["dir!"] = lfs.dir,
+	["lock!"] = lfs.lock,
+	["link!"] = lfs.link,
+	["mk-dir!"] = lfs.mkdir,
+	["rm-dir!"] = lfs.rmdir,
+	["set-mode!"] = lfs.setmode,
+	["symlink-attributes!"] = lfs.symlinkattributes,
+	["touch!"] = lfs.touch,
+	["unlock!"] = lfs.unlock
+}
---
+++ gen-bindings.lisp
@@ -0,0 +1,18 @@
+(import lua/basic (load))
+(import lua/table (sort))
+(import extra/io (read-all! write-lines!))
+(import string (sub))
+
+(defun gen-bindings (lua-code)
+  (let* [(bind-table ((load lua-code)))
+	 (bindings (keys bind-table))
+         (bind (lambda (name)
+           (.. "(define-native " name ")")))]
+    (sort bindings)
+    (map bind bindings)))
+
+(defun write-bindings! (infile outfile)
+  (write-lines! outfile (snoc (gen-bindings (read-all! infile)) "")))
+
+(when (> (# arg) 0)
+  (write-bindings! (car arg) (.. (sub (car arg) 1 -5) ".lisp")))
